9510fdf80bd1bd9c69acd8e7b38d683a3de19e6c
Vishnu Priya VR <vpriya@provensolution.com>
2021-09-27 20:52:33 +0530

    Indic Vision UI

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from easyocr_utils import show_uploaded_file, ocr_reader, read_image, language_mapping
 
-st.title("Indic OCR - OCR for Indian Languages")
+st.title("Indic Vision UI - OCR for Indian Languages")
 st.caption("Powered by EasyOCR and Streamlit")
 st.caption("Made with opensource ❤️ by @whisperingwasps")
 
@@ -30,13 +30,13 @@ if is_image_uploaded:
     st.sidebar.markdown("***")
 
     tooltip_markdown = '''
-        Select the language to perform Indic OCR on
+        Select the Indic language to perform OCR on
     '''.strip()
 
     languages_selected = st.sidebar.multiselect('Choose the language to perform OCR', ["Hindi", "Tamil", "Telugu", "Kannada", "Bihari", "Bhojpuri", "Bengali", "Goan Konkani", "Marathi", "Urdu" ], help=tooltip_markdown)
     
     if languages_selected:
-        all_languages_selected = st.sidebar.button("Perform Indic OCR")
+        all_languages_selected = st.sidebar.button("Perform OCR on the Indic language")
 
     #st.write(languages_selected)
     #st.write(all_languages_selected)
@@ -55,11 +55,11 @@ if is_image_uploaded:
                 ocr_reader_model = ocr_reader(lang_list)
                 ocr_result = read_image(ocr_reader_model,open_cv_image)
                 easyocr_bounding_process_status = True
-                st.success("Indic OCR completed successfully!")
+                st.success("OCR on the Indic language completed successfully!")
                 st.balloons()
             
     if loading_ocr_complete is True:
-        with st.expander("View Indic OCR results", expanded=False):
+        with st.expander("View OCR results", expanded=False):
             st.info("Column Details: 0 -> Bounding Box Co-ordinates, 1 -> Detected Text, 2 -> Detection Confidence")
             cropped_text_status = st.table(ocr_result)
 
